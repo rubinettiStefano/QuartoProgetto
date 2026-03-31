e3cde8cf33e341579d22ac6269d322eab4905051
StefanoR <rubinetti.stefano@gmail.com>
2026-03-31 16:50:17 +0200

    a voi

---
+++ src/letturaoggettifile/LettorePersone.java
@@ -0,0 +1,51 @@
+package letturaoggettifile;
+
+import entities.Persona;
+import libreria.Console;
+import libreria.FileReader;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class LettorePersone {
+
+    static void main() {
+
+        FileReader fr = new FileReader("dati.txt");
+        ArrayList<String> righe = new ArrayList<>();
+
+        while (fr.hasNextLine())
+            righe.add(fr.readString());
+
+        ArrayList<Persona> persone = new ArrayList<>();
+
+        for(String riga : righe)
+        {
+            String[] splittato = riga.split(",");
+            int convertita = Integer.parseInt(splittato[2]);
+            Persona p = new Persona(
+                    splittato[0],
+                    splittato[1],
+                    convertita
+            );
+            persone.add(p);
+        }
+
+        Console.print("TERMINA");
+
+    }
+}
+/*
+        String esempioRiga = "Stefano,Rubinetti,30";
+        String[] splittato = esempioRiga.split(",");
+        //splittato[0] = "Stefano";
+        //splittato[1] = "Rubinetti";
+        //splittato[2] = "30";
+        int convertita = Integer.parseInt(splittato[2]);
+        //se fosse un double allora double convertita = Double.parseDouble(splittato[2]);
+        Persona p = new Persona(
+                splittato[0],
+                splittato[1],
+                convertita
+        );
+ */
