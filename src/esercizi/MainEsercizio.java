fd490b71ff6bcba6932f394f0879c56b43fc2c45
StefanoR <rubinetti.stefano@gmail.com>
2026-03-31 17:00:50 +0200

    a voi

---
+++ src/esercizi/MainEsercizio.java
@@ -0,0 +1,10 @@
+package esercizi;
+
+//Leggendo i dati da datitelefoni.txt riempire una lista di telefoni
+//e poi scorrerla per stampare il toString di ognuno
+public class MainEsercizio
+{
+    static void main() {
+
+    }
+}
---
+++ src/esercizi/Telefono.java
@@ -0,0 +1,6 @@
+package esercizi;
+
+//proprieta -> Marca Modello Prezzo
+//metodo -> toString che dica "Telefono di marca Apple, Modello Iphone16, Prezzo 1100"
+public class Telefono {
+}
